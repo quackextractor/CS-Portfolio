7a3ce7d8c0b6eec03fde5ae868fbbe8a23e30375
LostSoul <miro.slezak0601@gmail.com>
2026-01-16 17:08:15 +0100

    Centralize target framework in Directory.Build.props
    Moved <TargetFramework> definition from individual .csproj files to a shared Directory.Build.props file for easier management. Added a batch script to update the target framework version across the solution.

---
+++ P2P/change_target_net_framework.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal
+
+:: Default to net8.0 if no argument is provided
+set "CURRENT_VERSION=net8.0"
+
+echo Current target framework is likely set in src\Directory.Build.props.
+echo Enter the new target framework (e.g., net8.0, net9.0).
+echo Press Enter to use default [%CURRENT_VERSION%]: 
+
+set /p NEW_VERSION=
+
+if "%NEW_VERSION%"=="" set NEW_VERSION=%CURRENT_VERSION%
+
+echo Updating target framework to %NEW_VERSION%...
+
+(
+echo ^<Project^>
+echo   ^<PropertyGroup^>
+echo     ^<TargetFramework^>%NEW_VERSION%^</TargetFramework^>
+echo   ^</PropertyGroup^>
+echo ^</Project^>
+) > src\Directory.Build.props
+
+echo Done. verify by running 'dotnet build src'
+pause
